5787a02a5d759678daa6c1040164c12481c9a920
Bill <tabebill09@gmail.com>
2023-10-28 01:28:15 +0100

    updated beanstalk codebuild

--- terraform/modules/beanstalk-codebuild/codebuild.tf
+++ terraform/modules/beanstalk-codebuild/codebuild.tf
@@ -1,3 +1,26 @@
+resource "aws_iam_role" "codebuild_service_role" {
+  name = "codebuild_service_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_service_role_policy_attachment" {
+  role = aws_iam_role.codebuild_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/CodeBuildServiceRolePolicy"
+}
+
 resource "aws_codebuild_project" "my_codebuild_project2" {
   name = "my-codebuild-project2"
   description = "My Beanstalk CodeBuild Project"
@@ -13,7 +36,7 @@ resource "aws_codebuild_project" "my_codebuild_project2" {
 
     environment_variable {
       name  = "AWS_DEFAULT_REGION"
-      value = var.aws_default_region
+      value = var.region
     }
   }
 
