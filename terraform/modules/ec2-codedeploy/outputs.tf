a7c020eb9c5577a07fad934548d1ba2de71ea297
Bill <tabebill09@gmail.com>
2023-10-30 05:27:23 +0100

    updated modules

--- terraform/modules/ec2-codedeploy/outputs.tf
+++ terraform/modules/ec2-codedeploy/outputs.tf
@@ -7,3 +7,13 @@ output "aws_codedeploy_deployment_group_arn" {
   description = "aws_codedeploy_deployment_group"
   value       = aws_codedeploy_deployment_group.my_deployment_group.arn
 }
+
+output "aws_codedeploy_app" {
+  description = "aws codedeploy app name"
+  value       = aws_codedeploy_app.my_app.name
+}
+
+output "aws_codedeploy_deployment_group_name" {
+  description = "aws codedeploy group name"
+  value       = aws_codedeploy_deployment_group.my_deployment_group.deployment_group_name
+}
