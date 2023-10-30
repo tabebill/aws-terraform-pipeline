04dcf969d9cd414c44c88ff02efab923d579948f
Bill <tabebill09@gmail.com>
2023-10-30 05:24:15 +0100

    updated modules

--- terraform/modules/beanstalk-codebuild/outputs.tf
+++ terraform/modules/beanstalk-codebuild/outputs.tf
@@ -1,4 +1,4 @@
-output "aws_codebuild_project_arn" {
-  value       = aws_codebuild_project.my_codebuild_project2.arn
-  description = "aws codebuild project arn"
+output "codebuild_project2" {
+  value       = aws_codebuild_project.my_codebuild_project2.name
+  description = "aws codebuild project name"
 }
