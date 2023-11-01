ca17e3e6b8ed2cf20334a10ca491744721f4db1e
Bill <tabebill09@gmail.com>
2023-11-01 01:48:34 +0100

    modified vpc outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -3,9 +3,14 @@ output "vpc_id" {
   value       = aws_vpc.my_vpc.id
 }
 
-output "public_subnet" {
+output "public_subnet1" {
   description = "Will be used by Web Server Module to set subnet_ids"
-  value = aws_subnet.public_subnet.id
+  value = aws_subnet.public_subnet1.id
+}
+
+output "public_subnet2" {
+  description = "Will be used by Web Server Module to set subnet_ids"
+  value = aws_subnet.public_subnet2.id
 }
 
 output "my_asg_arn" {
